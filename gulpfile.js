c19ffb564c4492f2ada7f2a8a8197a8e7fc768c5
Nanako Mori <nanako.mori19871229@gmail.com>
2021-07-30 11:50:06 +0900

    ディレクトリ構成を修正

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass')(require('sass'));
 
 gulp.task('sass', function() {
-  return gulp.src('./sass/**/*.scss')
+  return gulp.src('./static/sass/*.scss')
     .pipe(sass({outputStyle: 'expanded'}))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./static/css'));
 });
 
 gulp.task('sass:watch', function() {
-  gulp.watch('./sass/**/*.scss', gulp.task('sass'));
+  gulp.watch('./static/sass/*.scss', gulp.task('sass'));
 });
